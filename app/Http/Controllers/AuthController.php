d770d6a4f0912ebbf63a5488d258a1e05b7f6300
harsh-agr79 <agrharsh7932@gmail.com>
2025-09-06 22:41:15 +0530

    first commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
         $validator = Validator::make( $request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:customers',
-            'password' => [ 'required', 'string', 'min:8', 'regex:/[ A-Za-z ]/', 'regex:/[ 0-9 ]/', 'regex:/[ @$!%*#?& ]/' ],
+            'password' => [ 'required', 'string', 'min:8' ],
         ] );
 
         if ( $validator->fails() ) {
