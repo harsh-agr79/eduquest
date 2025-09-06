0422437799de7f95c10d808a1b11dcbcced2b12c
harsh-agr79 <agrharsh7932@gmail.com>
2025-09-06 20:52:59 +0530

    first commit

--- database/migrations/2025_09_06_144505_add_xp_field_to_customer.php
+++ database/migrations/2025_09_06_144505_add_xp_field_to_customer.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->bigInteger('xp')->default(100);
+            $table->integer('xp')->default(0);
         });
     }
 
